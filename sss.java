ad95652e851e57c42ffd90e123a6f626ff3c51e4
12gouravss <79088594+12gouravss@users.noreply.github.com>
2024-02-09 09:33:45 +0530

    Create sss.java

---
+++ sss.java
@@ -0,0 +1,25 @@
+package sseleniums;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+public class sss{
+
+	public static void main(String[] args) throws InterruptedException{
+		
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Gourav S S\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		ChromeDriver driver =new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.vvce.ac.in/");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		//driver.findElement(By.xpath("//span[text()='x'][1]")).click();
+		System.out.println(driver.getTitle());
+		driver.quit();
+		
+
+
+	}
+
+}
